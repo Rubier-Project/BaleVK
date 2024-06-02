5036475a74107dbba233bc41aed7e13f6b97782a
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-06-02 20:52:45 +0400

    Create chatGpt.py

---
+++ objects/chatGpt.py
@@ -0,0 +1,53 @@
+from main import BaleVK
+from formattingText import formatObject
+from forefront import ForefrontClient
+from forefront.utils import SystemChat, UserChat
+
+ff = ForefrontClient(api_key="sk-mHnr4wJ44XpzqGGLa5Dg5pjVfTsRsNYu")
+
+def gpt(user: str):
+    completion = ff.chat.completions.create(
+        messages=[
+            SystemChat("You are a helpful assistant"),
+            UserChat(user),
+        ],
+        model="mistralai/Mistral-7B-v0.1",
+        temperature=0.5,
+        max_tokens=128,
+    )
+    return str(completion.message['content'])
+
+app = BaleVK("1422238319:5PgfM2jYAfiXQndgBWwOgxmRyce4DLomzrIhhb8W")
+msg_lis = set()
+
+print("Start")
+
+while 1:
+    for msg in app.on(
+        "message"
+    ):
+        text = msg.text
+        chat = msg.chat_id
+        msg_id = msg.message_id
+
+        if not msg_id in msg_lis:
+            msg_lis.add(msg_id)
+            print("Text Recived:", text, f":{chat}")
+
+            if text == "/start":
+                app.sendMessage(chat, "type /help to see options", msg_id)
+
+            if text == "/help":
+                app.sendMessage(chat, "Write your order or question in front of command /gpt\n  /gpt Hello can you help Me?", msg_id)
+
+            if text.startswith("/gpt"):
+                txt = text.replace("/gpt ", "")
+
+                if txt == "/gpt" or txt == " ":
+                    app.sendMessage(chat, "Faild to Use, to see usage type {}".format(formatObject('/help').add_info('Write your order or question in front of command /gpt\n  /gpt Hello can you help Me?')), msg_id)
+                else:
+                    uinion = app.sendMessage(chat, "Wait a Second ...", msg_id)
+                    info = gpt(txt)
+                    app.editMessageText(chat, info, uinion['result']['message_id'])
+        else:
+            msg_lis.add(msg_id)
